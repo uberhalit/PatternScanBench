b8e41ce52e2c9c648608e0ddf31370d4e222ec5f
hollow87 <hollow87@users.noreply.github.com>
2019-04-22 16:04:13 -0500

    Using nuget instead of repository provided assemblies (#1)
    * System.Numerics.Vectors is used from nuget now
    
    * Added nuget package MSBuild.AutoILMerge.Task by Kim Christensen to reproduce the single exe build.

--- PatternScanBench/App.cs
+++ PatternScanBench/App.cs
@@ -10,26 +10,6 @@ namespace PatternScanBench
         [STAThreadAttribute]
         public static void Main()
         {
-            AppDomain.CurrentDomain.AssemblyResolve += (s, args) =>
-            {
-                Assembly assembly = Assembly.GetExecutingAssembly();
-                var embeddedResources = new List<string>(assembly.GetManifestResourceNames());
-                string assemblyName = new AssemblyName(args.Name).Name;
-                string fileName = string.Format("{0}.dll", assemblyName);
-                string resourceName = embeddedResources.FirstOrDefault(ern => ern.EndsWith(fileName));
-                if (!string.IsNullOrWhiteSpace(resourceName))
-                {
-                    using (var stream = assembly.GetManifestResourceStream(resourceName))
-                    {
-                        Byte[] assemblyData = new Byte[stream.Length];
-                        stream.Read(assemblyData, 0, assemblyData.Length);
-                        return Assembly.Load(assemblyData);
-                    }
-                }
-
-                return null;
-            };
-
             Program.Init(null);
         }
     }
